1118f81586869a69c4bdd613e3a89f2bd106eed5
fkmbc <mail@florian-kittel.de>
2017-07-30 21:59:21 +0200

    feat(form-creator): add basic element for form creator

--- src/app/pages/sign-up/sign-up.component.ts
+++ src/app/pages/sign-up/sign-up.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormArray, FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
@@ -7,9 +8,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignUpComponent implements OnInit {
 
+  cards = [
+    {
+      title: 'Personal Informations',
+      text: 'Please enter your Personal Informations.'
+    },
+    {
+      title: 'User Account data',
+      text: 'Please enter username and password for login.'
+    }
+  ]
+
+  signupForm: FormGroup;
+
+  inputs = [
+    {
+      key: 'firstName',
+      label: 'First name',
+      value: 'Bombasto',
+      required: true,
+      order: 1
+    }
+  ]
+
+  toFormGroup(inputs) {
+    const group: any = {};
+
+    inputs.forEach(input => {
+      group[input.key] = input.required ? new FormControl(input.value || '', Validators.required) : new FormControl(input.value || '');
+    });
+
+    return new FormGroup(group);
+  }
+
   constructor() { }
 
   ngOnInit() {
+    this.signupForm = this.toFormGroup(this.inputs);
   }
 
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,17 +3,25 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
 import { NgxErrorsModule } from '@ultimate/ngxerrors';
+import { ContentCardComponent } from './content-card/content-card.component';
+import { InputsModule } from './inputs/inputs.module';
 
 
 export const sharedModules = [
   CommonModule,
   FormsModule,
+  InputsModule,
   NgxErrorsModule,
   ReactiveFormsModule,
 ]
 
+export const sharedComponents = [
+  ContentCardComponent,
+]
+
 @NgModule({
   imports: [...sharedModules],
-  exports: [...sharedModules]
+  exports: [...sharedModules, ...sharedComponents],
+  declarations: [...sharedComponents]
 })
 export class SharedModule { }
